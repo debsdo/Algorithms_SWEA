a2943241366f0090738c97af18c54321b41a7055
informuscle <infomuscle10@gmail.com>
2018-08-30 20:22:35 +0900

    연월일 달력

--- 2056.c
+++ 2056.c
@@ -1,50 +1,103 @@
-﻿#include <stdio.h>
+﻿//연월일 순으로 구성된 8자리의 날짜가 입력으로 주어진다.
+//해당 날짜의 유효성을 판단한 후, 날짜가 유효하다면
+//[그림1] 과 같이 ”YYYY / MM / DD”형식으로 출력하고,
+//날짜가 유효하지 않을 경우, -1 을 출력하는 프로그램을 작성하라.
+//연월일로 구성된 입력에서 월은 1~12 사이 값을 가져야 하며
+//일은[표1] 과 같이, 1일 ~각각의 달에 해당하는 날짜까지의 값을 가질 수 있다.
+//※ 2월의 경우, 28일인 경우만 고려한다. (윤년은 고려하지 않는다.)
+
+//[입력]
+//입력은 첫 줄에 총 테스트 케이스의 개수 T가 온다.
+//다음 줄부터 각 테스트 케이스가 주어진다.
+
+//[출력]
+//테스트 케이스 t에 대한 결과는 “#t”을 찍고, 한 칸 띄고, 정답을 출력한다.
+//(t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
+
+#include <stdio.h>
+
+int mChker(int num) {
+	if (num >= 1 && num <= 12) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int mGrpFnder(int mt) {
+	char m30[4] = { 4,6,9,11 };
+	char m31[7] = { 1,3,5,7,8,10,12 };
+	char m28[1] = { 2 };
+	char mChk = mChker(mt);
+
+	if (mChk == 1) {
+		for (int i = 0; i < 4; i++) {
+			if (mt == m30[i]) {
+				return 30;
+			}
+		}
+
+		for (int i = 0; i < 7; i++) {
+			if (mt == m31[i]) {
+				return 31;
+			}
+		}
+
+		return 28;
+	}
+	else {
+		return 0;
+	}
+}
+
+int dChker(int mt, int dt) {
+	char mGrp = mGrpFnder(mt);
+	if (mGrp == 30) {
+		if (dt >= 1 && dt <= 30) {
+			return 1;
+		}
+	}
+	else if (mGrp == 31) {
+		if (dt >= 1 && dt <= 31) {
+			return 1;
+		}
+	}
+	else if (mGrp == 28) {
+		if (dt >= 1 && dt <= 28) {
+			return 1;
+		}
+	}
+	else {
+		return 0;
+	}
+}
 
 int main() {
-    int T, tc = 0;
-    scanf("%d", &T);
-    
-    while (tc++ < T) {
-        int inNum, yr, mt, dt;
-        scanf("%d", &inNum);
-        yr = inNum / 10000;
-        mt = (inNum % 10000) / 100;
-        dt = (inNum % 10000) % 100;
-        
-        int flag = 0;
+	int tc, inNum;
 
-        while(1){
-            if (mt <= 0 || mt > 12) {
-                flag = 1;
-                break;
-			}
-            if (mt == 2) {
-                if (dt <= 0 || dt > 28) {
-                    flag = 1;
-                    break;
-                }
-            }
-            else if (mt == 4 || mt == 6 || mt == 9 || mt == 11) {
-                if (dt <= 0 || dt > 30) {
-                    flag = 1;
-                    break;
-				}
-			}
-			else {
-				if (dt <= 0 || dt > 31) {
-					flag = 1;
-					break;
-				}
-			}
-			break;
+	scanf("%d", &tc);
+
+	for (int i = 0; i < tc; i++) {
+		int yr, mt, dt;
+
+		scanf("%d", &inNum);
+
+		yr = inNum / 10000;
+		mt = (inNum % 10000) / 100;
+		dt = (inNum % 10000) % 100;
+
+		char mChk = mChker(mt);
+		char dChk = dChker(mt, dt);
+
+		if (mChk == 1 && dChk == 1) {
+			printf("#%d ", i + 1);
+			printf("%04d/%02d/%02d\n", yr, mt, dt);
 		}
-      
-      if (flag) {
-         printf("#%d -1\n", tc);
-      }
-      else {
-          printf("#%d %04d/%02d/%02d\n", tc, yr, mt, dt);
-	  }
-   }
-   return 0;
+		else {
+			printf("#%d -1\n", i + 1);
+		}
+	}
+
+	return 0;
 }
