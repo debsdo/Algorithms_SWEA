16f5733880c2935dea1a7de5fa45f642db581a54
informuscle <infomuscle10@gmail.com>
2018-08-31 13:42:52 +0900

    스탬프 찍기

---
+++ 2046.c
@@ -0,0 +1,14 @@
+﻿//주어진 숫자만큼 # 을 출력해보세요.
+//주어질 숫자는 100, 000 이하다.
+
+#include <stdio.h>
+
+int main() {
+	int pn;
+	scanf("%d", &pn);
+	for (int i = 0; i < pn; i++) {
+		printf("#");
+	}
+
+	return 0;
+}
