e11148e991cefca6ec8001184ef2b1be9be62b0d
informuscle <infomuscle10@gmail.com>
2018-08-31 21:48:39 +0900

    간단한 369게임

---
+++ 1926.c
@@ -0,0 +1,76 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int num;
+	scanf("%d", &num);
+
+	for (int i = 1; i <= num; i++) {
+		if (i < 10) {
+			if (i == 3 || i == 6 || i == 9) {
+				printf("- ");
+			}
+			else {
+				printf("%d ", i);
+			}
+		}
+		else if (i < 100) {
+			char charNum[3];
+			sprintf(charNum, "%d", i);
+			if (charNum[0] == '3' || charNum[0] == '6' || charNum[0] == '9') {
+				if (charNum[1] == '3' || charNum[1] == '6' || charNum[1] == '9') {
+					printf("-- ");
+				}
+				else {
+					printf("- ");
+
+				}
+			} else if (charNum[1] == '3' || charNum[1] == '6' || charNum[1] == '9') {
+				printf("- ");
+			}
+			else {
+				printf("%d ", i);
+			}
+		}
+		else if (i < 1000) {
+			char charNum[4];
+			sprintf(charNum, "%d", i);
+			if (charNum[0] == '3' || charNum[0] == '6' || charNum[0] == '9') {
+				if (charNum[1] == '3' || charNum[1] == '6' || charNum[1] == '9') {
+					if (charNum[2] == '3' || charNum[2] == '6' || charNum[2] == '9') {
+						printf("--- ");
+					}
+					else {
+						printf("-- ");
+					}
+				}
+				else if (charNum[2] == '3' || charNum[2] == '6' || charNum[2] == '9') {
+					printf("-- ");
+				}
+				else {
+					printf("- ");
+
+				}
+			}
+			else if (charNum[1] == '3' || charNum[1] == '6' || charNum[1] == '9') {
+				if (charNum[2] == '3' || charNum[2] == '6' || charNum[2] == '9') {
+					printf("-- ");
+				}
+				else {
+					printf("- ");
+				}
+			}
+			else if (charNum[2] == '3' || charNum[2] == '6' || charNum[2] == '9') {
+				printf("- ");
+			}
+			else {
+				printf("%d ", i);
+			}
+		}
+		else {
+			printf("%d", i);
+		}
+	}
+	
+	return 0;
+}
