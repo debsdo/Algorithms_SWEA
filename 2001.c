2125e55827bb86a737d79b140f6291c65d73cf70
informuscle <infomuscle10@gmail.com>
2018-09-04 19:23:42 +0900

    파리 퇴치

--- 2001.c
+++ 2001.c
@@ -1,12 +1,32 @@
-﻿#include <stdio.h>
+﻿// N x N 배열 안의 숫자는 해당 영역에 존재하는 파리의 개수를 의미한다.
+// 아래는 N=5 의 예이다.
+// M x M 크기의 파리채를 한 번 내리쳐 최대한 많은 파리를 죽이고자 한다.
+// 죽은 파리의 개수를 구하라!
+// 예를 들어 M=2 일 경우 위 예제의 정답은 49마리가 된다.
+
+// [제약 사항]
+// 1. N 은 5 이상 15 이하이다.
+// 2. M은 2 이상 N 이하이다.
+// 3. 각 영역의 파리 갯수는 30 이하 이다.
+
+// [입력]
+// 가장 첫 줄에는 테스트 케이스의 개수 T가 주어지고, 그 아래로 각 테스트 케이스가 주어진다.
+// 각 테스트 케이스의 첫 번째 줄에 N 과 M 이 주어지고,
+// 다음 N 줄에 걸쳐 N x N 배열이 주어진다.
+
+// [출력]
+// 출력의 각 줄은 '#t'로 시작하고, 공백을 한 칸 둔 다음 정답을 출력한다.
+// (t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
+
+#include <stdio.h>
 #include <malloc.h>
 
-int killCounter(char **row, int rowStart, int colStart, int m) {
+int killCounter(char **array, int rowStart, int colStart, int m) {
 	int killNum = 0;
 	
 	for (int i = rowStart; i < rowStart + m; i++) {
 		for (int j = colStart; j < colStart + m; j++) {
-			killNum += row[i][j];
+			killNum += array[i][j];
 		}
 	}
 
@@ -21,22 +41,22 @@ int main() {
 	for (int t = 0; t < tc; t++) {
 		int n, m, temp;
 		int kill = 0;
-		char **row;
+		char **array;
 
 		scanf("%d %d", &n, &m);
 
-		row = (char **)malloc(sizeof(char)*n);
+		array = (char **)malloc(sizeof(char*)*n);
 
 		for (int i = 0; i < n; i++) {
-			row[i] = (char*)malloc(sizeof(char)*n);
+			array[i] = (char*)malloc(sizeof(char)*n);
 			for (int j = 0; j < n; j++) {
-				scanf("%d", &row[i][j]);
+				scanf("%d", &array[i][j]);
 			}
 		}
 
 		for (int i = 0; i < n - m + 1; i++) {
 			for (int j = 0; j < n - m + 1; j++) {
-				temp = killCounter(row, i, j, m);
+				temp = killCounter(array, i, j, m);
 				if (temp > kill) {
 					kill = temp;
 				}
