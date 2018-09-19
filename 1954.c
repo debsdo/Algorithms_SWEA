43ce65783576ebfdb6036fa7c18b6fa049ff9e14
infomuscle <infomuscle10@gmail.com>
2018-09-19 13:04:54 +0900

    달팽이 숫자

--- 1954.c
+++ 1954.c
@@ -1,4 +1,22 @@
-﻿#include <stdio.h>
+﻿// 달팽이는 1부터 N*N까지의 숫자가 시계방향으로 이루어져 있다.
+// 다음과 같이 정수 N을 입력 받아 N크기의 달팽이를 출력하시오.
+
+// [예제]
+// N이 3일 경우,
+// N이 4일 경우,
+
+// [제약사항]
+// 달팽이의 크기 N은 1 이상 10 이하의 정수이다. (1 ≤ N ≤ 10)
+
+// [입력]
+// 가장 첫 줄에는 테스트 케이스의 개수 T가 주어지고, 그 아래로 각 테스트 케이스가 주어진다.
+// 각 테스트 케이스에는 N이 주어진다.
+
+// [출력]
+// 각 줄은 '#t'로 시작하고, 다음 줄부터 빈칸을 사이에 두고 달팽이 숫자를 출력한다.
+// (t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
+
+#include <stdio.h>
 #include <malloc.h>
 
 int main() {
@@ -23,55 +41,95 @@ int main() {
 			}
 		}
 
-		int num = 0, row = 0, col = -1;
-		int right = 1, down = 0, left = 0, up = 0;
+		int num = 0, row = 0, col = -1, cnt = n;
+		// right down left up
+		int way[4] = { 1,0,0,0 };
 
-		while (num < n*n) {
-			num += 1;
-			
-			if (right) {
-				col += 1;
-			}
-			else if (down) {
-				row += 1;
-			}
-			else if (left) {
-				col -= 1;
-			}
-			else if (up) {
-				row -= 1;
-			}
-
-			if (snail[row][col] == 0) {
-				snail[row][col] = num;
-			}
-			else {
-				if (right) {
-					right = 0;
-					down = 1;
-					num -= 1;
-					col -= 1;
-				}
-				else if (down) {
-					down = 0;
-					left = 1;
-					num -= 1;
-					row -= 1;
-				}
-				else if (left) {
-					left = 0;
-					up = 1;
-					num -= 1;
+		for (int k = 0; k < n - 1; k++) {
+			for (int i = 0; i < cnt; i++) {
+				if (way[0] == 1) {
 					col += 1;
 				}
-				else if (up) {
-					up = 0;
-					right = 1;
-					num -= 1;
+				else if (way[1] == 1) {
 					row += 1;
 				}
+				else if (way[2] == 1) {
+					col -= 1;
+				}
+				else if (way[3] == 1) {
+					row -= 1;
+				}
+				num += 1;
+				snail[row][col] = num;
 			}
+
+			if (way[0] == 1) {
+				way[0] = 0;
+				way[1] = 1;
+			}
+			else if (way[1] == 1) {
+				way[1] = 0;
+				way[2] = 1;
+			}
+			else if (way[2] == 1) {
+				way[2] = 0;
+				way[3] = 1;
+			}
+			else if (way[3] == 1) {
+				way[3] = 0;
+				way[0] = 1;
+			}
+
+			for (int i = 0; i < cnt - 1; i++) {
+				if (way[0] == 1) {
+					col += 1;
+				}
+				else if (way[1] == 1) {
+					row += 1;
+				}
+				else if (way[2] == 1) {
+					col -= 1;
+				}
+				else if (way[3] == 1) {
+					row -= 1;
+				}
+				num += 1;
+				snail[row][col] = num;
+			}
+
+			if (way[0] == 1) {
+				way[0] = 0;
+				way[1] = 1;
+			}
+			else if (way[1] == 1) {
+				way[1] = 0;
+				way[2] = 1;
+			}
+			else if (way[2] == 1) {
+				way[2] = 0;
+				way[3] = 1;
+			}
+			else if (way[3] == 1) {
+				way[3] = 0;
+				way[0] = 1;
+			}
+			cnt -= 1;
 		}
+
+		if (way[0] == 1) {
+			col += 1;
+		}
+		else if (way[1] == 1) {
+			row += 1;
+		}
+		else if (way[2] == 1) {
+			col -= 1;
+		}
+		else if (way[3] == 1) {
+			row -= 1;
+		}
+		num += 1;
+		snail[row][col] = num;
 
 		printf("#%d\n", t + 1);
 		for (int i = 0; i < n; i++) {
@@ -80,12 +138,8 @@ int main() {
 			}
 			printf("\n");
 		}
-
-		for (int i = 0; i < n+1; i++) {
-			free(snail[i]);
-		}
-		free(snail);
 	}
 
 	return 0;
 }
+
