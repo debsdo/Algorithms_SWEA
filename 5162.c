8e3e09045b6399ab53d131adf2a84bc7436ab89c
infomuscle <infomuscle10@gmail.com>
2018-10-09 13:09:20 +0900

    두가지 빵의 딜레마

---
+++ 5162.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main() {
+	int tc;
+	scanf("%d", &tc);
+
+	for (int t = 0; t < tc; t++) {
+		int a, b, c;
+		scanf("%d %d %d", &a, &b, &c);
+		
+		int breads = 0;
+		int x = 0;
+		int y = 0;
+
+		if (a < b) {
+			x = c / a;
+			y = (c%a) / b;
+		}
+		else {
+			y = c / b;
+			x = (c%b) / a;
+		}
+		breads = x + y;
+		printf("#%d %d\n", t + 1, breads);
+	}
+
+	return 0;
+}
