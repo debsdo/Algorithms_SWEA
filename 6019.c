7424b96d7610a6b67b6629320c168fd5bce43bb6
infomuscle <infomuscle10@gmail.com>
2018-11-10 22:58:27 +0900

    기차 사이의 파리 - 인터넷 오류로 채점 불가

---
+++ 6019.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+	int tc;
+	scanf("%d", &tc);
+
+	for (int t = 0; t < tc; t++) {
+		int d, a, b, f;
+		scanf("%d%d%d%d", &d, &a, &b, &f);
+
+		double x = d / (a + b);
+
+		printf("#%d %lf\n", t + 1, (double)f*x);
+
+	}
+
+	return 0;
+}
